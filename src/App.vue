bd9882c3619d0e1397c2692155bdb60391776456
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-25 11:39:49 +0100

    add logo and navbar to SiteHeader

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div id="app">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <SiteHeader />
+    <SiteHeader :navMenu="navMenuOptions" />
     <SiteMain />
     <SiteFooter />
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
 import SiteHeader from "./components/SiteHeader.vue";
 import SiteMain from "./components/SiteMain.vue";
 import SiteFooter from "./components/SiteFooter.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      navMenuOptions: [
+        "Home",
+        "About Us",
+        "Facilities",
+        "Membership",
+        "Testimonials",
+        "Blog",
+      ],
+    };
+  },
   components: {
-    // HelloWorld
     SiteHeader,
     SiteMain,
     SiteFooter,
@@ -25,4 +34,5 @@ export default {
 </script>
 
 <style lang="scss">
+@import "./assets/scss/common.scss";
 </style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,10 +1,64 @@
 <template>
-  <header></header>
+  <header>
+    <div class="logo">
+      <img src="../assets/images/gym_logo_1x.png" alt="" />
+    </div>
+    <nav>
+      <ul>
+        <li
+          v-for="(option, index) in navMenu"
+          :key="option"
+          :class="active != index ? '' : 'active'"
+          @click="active = index"
+        >
+          <a href="#">{{ option.toUpperCase() }}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      active: 0,
+    };
+  },
+  props: {
+    navMenu: Array,
+  },
+};
 </script>
 
+
 <style lang="scss">
+@import "../assets/scss/variables.scss";
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2.5rem 4rem;
+  background-color: $darker-bg-color;
+  nav {
+    color: $primary-text-color;
+    ul {
+      display: flex;
+      list-style: none;
+      li {
+        margin: 0 1rem;
+        &.active {
+          a {
+            color: $active-text-color;
+          }
+        }
+        a {
+          text-decoration: none;
+          color: $primary-text-color;
+        }
+      }
+    }
+  }
+}
 </style>
