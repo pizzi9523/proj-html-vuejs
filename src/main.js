a4b5971a7ffa9dcedb1e8fb8203002a0f9b9caba
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-26 13:23:58 +0100

    fix font family

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "@fontsource/open-sans"
+import "@fontsource/montserrat"
+import "@fontsource/open-sans/700.css"
+
 
 Vue.config.productionTip = false
 
