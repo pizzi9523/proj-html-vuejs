13c533fb1160dc7a26c1c254226586b77c741a54
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-26 15:33:58 +0100

    fix backgorund Banner in Banner component

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -23,7 +23,7 @@ section.testimonial {
   background-image: url("../assets/images/home-testimonial-bg.jpg");
   background-position-y: center;
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: 100% 1240px;
 }
 .banner {
   display: flex;
@@ -33,13 +33,14 @@ section.testimonial {
     text-align: center;
 
     p.author_cit {
-      padding: 40px 0 170px 0;
+      padding: 40px 0 200px 0;
     }
   }
   p.cit {
+    width: 700px;
     font-size: 30px;
     line-height: 50px;
-    padding: 170px 0 40px 0;
+    padding: 200px 0 40px 0;
   }
 }
 </style>
